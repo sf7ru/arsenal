dcaa679a1efb71afcdc4a6aa0e13438b070793cc
ak0 <akzhr@ya.ru>
2023-04-12 18:09:55 +0300

    C

---
+++ libax/include/pvt_win.h
@@ -0,0 +1,98 @@
+// ***************************************************************************
+// TITLE
+//      Win32 Private Header
+// PROJECT
+//      Arsenal Library
+// ***************************************************************************
+// FILE
+//      $Id: pvt_win.h,v 1.2 2003/05/09 09:33:01 A.Kozhukhar Exp $
+// AUTHOR
+//      $Author: A.Kozhukhar $
+// ***************************************************************************
+
+#ifndef __LA6WIN_PRIVATEH__
+#define __LA6WIN_PRIVATEH__
+
+#include <arsenal.h>
+
+
+// ---------------------------------------------------------------------------
+// ----------------------------- DEFINITIONS ---------------------------------
+// ---------------------------------------------------------------------------
+
+#define MAC_TIME_WIN2AX(a,b)   { a->year          = b->wYear;            \
+                                  a->month         = b->wMonth;           \
+                                  a->day_of_week   = b->wDayOfWeek;       \
+                                  a->day           = b->wDay;             \
+                                  a->hour          = b->wHour;            \
+                                  a->minute        = b->wMinute;          \
+                                  a->second        = b->wSecond;          \
+                                  a->milliseconds  = b->wMilliseconds;    }
+
+
+// ---------------------------------------------------------------------------
+// -------------------------------- TYPES ------------------------------------
+// ---------------------------------------------------------------------------
+
+// ***************************************************************************
+// STRUCTURE
+//      LA6PIPE
+// PURPOSE
+//
+// ***************************************************************************
+typedef struct __tag_LA6PIPE
+{
+    HANDLE              h_in;
+    HANDLE              h_out;
+} LA6PIPE, * PLA6PIPE;
+
+// ***************************************************************************
+// STRUCTURE
+//      WIN32WTIMER
+// PURPOSE
+//
+// ***************************************************************************
+typedef struct __tag_WIN32WTIMER
+{
+    HANDLE          h_timer;
+} WIN32WTIMER, * PWIN32WTIMER;
+
+
+// ***************************************************************************
+// STRUCTURE
+//      AXFILE
+// PURPOSE
+//
+// ***************************************************************************
+typedef struct __tag_AXFILE
+{
+    HANDLE          h_file;
+} AXFILE, * PAXFILE;
+
+// ---------------------------------------------------------------------------
+// ------------------------------ FUNCTIONS ----------------------------------
+// ---------------------------------------------------------------------------
+
+#ifdef __cplusplus
+extern "C" {
+#endif                                      //  #ifdef __cplusplus
+
+PVOID           la6la6_NTDLL_get_proc           (PSTR       psz_procname);
+
+int             _la6_nonblk_file_read           (HANDLE     h_file,
+                                                 PVOID      p_data,
+                                                 int        i_size,
+                                                 UINT       u_timeout_msec);
+
+int             _la6_nonblk_file_write          (HANDLE     h_file,
+                                                 PVOID      p_data,
+                                                 int        i_size,
+                                                 UINT       u_timeout_msec);
+
+
+#ifdef __cplusplus
+}
+#endif                                      //  #ifdef __cplusplus
+
+
+#endif                                      //  #ifndef __LA6WIN32_PRIVATEH__
