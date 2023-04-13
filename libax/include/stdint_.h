b6d8bce93adb6a3aa43a5c4b3cedcbe776047c57
Alexander Kozhukhar <ak@uloc.ru>
2023-04-13 12:04:39 +0300

    C

--- libax/include/stdint_.h
+++ libax/include/stdint_.h
@@ -15,8 +15,11 @@ typedef signed int int32_t;
 typedef unsigned short uint16_t;
 typedef signed short int16_t;
 
-typedef unsigned long long int64_t;
+typedef signed char int8_t;
 typedef unsigned char uint8_t;
+
+typedef signed long int64_t;
+typedef unsigned long uint64_t;
 
 typedef unsigned char bool;
 
